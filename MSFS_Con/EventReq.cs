a20715bf21550493ae10c36d2a55cc102128911c
Akio Tomita <tmyaymt@gmail.com>
2022-10-11 00:21:30 +0900

    Added SimConnect func and UDP fund.

---
+++ MSFS_Con/EventReq.cs
@@ -0,0 +1,21 @@
+﻿namespace MSFS_Con
+{
+    enum EVENTS
+    {
+        // If you want, add the values,
+        ALTITUDE_SLOT_INDEX_SET,
+        HEADING_SLOT_INDEX_SET,
+        SPEED_SLOT_INDEX_SET,
+        VS_SLOT_INDEX_SET,
+        FLAPS_INCR,
+        FLAPS_DECR,
+        AP_MASTER,
+        AP_SPD_VAR_SET,
+        AP_ALT_VAR_SET_ENGLISH,
+        HEADING_BUG_SET,
+    };
+    enum NOTIFICATION_GROUPS
+    {
+        Group0,
+    }
+}
