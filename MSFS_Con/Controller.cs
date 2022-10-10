92fac979ee916f65db8586a9ad3e7d54acac183e
Akio Tomita <tmyaymt@gmail.com>
2022-10-11 02:01:16 +0900

    Added interface skeleton of controller.

---
+++ MSFS_Con/Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSFS_Con
+{
+    class Controller
+    {
+        #region SimConnect interface
+
+        #endregion
+
+        #region Serial interface
+
+        #endregion
+
+        #region UDP interface
+
+        #endregion
+
+    }
+}
