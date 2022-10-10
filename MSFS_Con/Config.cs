4dc203a94da875a0933c5c4f89858eb5fc384575
meg <meg.diy.555@gmail.com>
2022-10-11 00:35:25 +0900

    Added serial communication function.

---
+++ MSFS_Con/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSFS_Con
+{
+    class Config
+    {
+        public static String com_name = "COM6";
+        public static int com_baudrate = 115200;
+    }
+}
--- MSFS_Con/Form1.Designer.cs
+++ MSFS_Con/Form1.Designer.cs
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             this.buttonConnect = new System.Windows.Forms.Button();
+            this.textBox_comname = new System.Windows.Forms.TextBox();
+            this.button_com_connect = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonConnect
@@ -41,22 +46,67 @@
             this.buttonConnect.UseVisualStyleBackColor = true;
             this.buttonConnect.Click += new System.EventHandler(this.button1_Click);
             // 
+            // textBox_comname
+            // 
+            this.textBox_comname.Location = new System.Drawing.Point(44, 82);
+            this.textBox_comname.Name = "textBox_comname";
+            this.textBox_comname.Size = new System.Drawing.Size(100, 19);
+            this.textBox_comname.TabIndex = 1;
+            this.textBox_comname.Text = "COM1";
+            this.textBox_comname.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox_comname.TextChanged += new System.EventHandler(this.textBox_comname_TextChanged);
+            // 
+            // button_com_connect
+            // 
+            this.button_com_connect.Location = new System.Drawing.Point(150, 82);
+            this.button_com_connect.Name = "button_com_connect";
+            this.button_com_connect.Size = new System.Drawing.Size(75, 23);
+            this.button_com_connect.TabIndex = 2;
+            this.button_com_connect.Text = "connect";
+            this.button_com_connect.UseVisualStyleBackColor = true;
+            this.button_com_connect.Click += new System.EventHandler(this.button_com_connect_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button_com_connect);
+            this.Controls.Add(this.textBox_comname);
             this.Controls.Add(this.buttonConnect);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.TextBox textBox_comname;
+        private System.Windows.Forms.Button button_com_connect;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
     }
 }
 
